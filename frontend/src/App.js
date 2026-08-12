fa363b2b1c7c96399f7eec429aa2b5f1c78fc20e
pushka2006 <pushkarbalyan14@gmail.com>
2026-08-13 00:25:11 +0530

    Fix Router closing tag syntax in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -152,7 +152,7 @@ function App() {
                   </Routes>
                 </Shell>
               </VoiceProvider>
-            </BrowserRouter>
+            </Router>
           </div>
         </div>
         </ErrorFixerProvider>
